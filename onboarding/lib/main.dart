5dc29384acfe16404bf8c4b049b91272ef1bf425
jsomnium <jsomnium@naver.com>
2022-11-10 15:14:22 +0900

    add Shared_Preferences

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  // main() 함수에서 async를 쓰려면 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // shared_preferences 인스턴스 생성
+  SharedPreferences prefs = await SharedPreferences.getInstance();
 }
 
 class MyApp extends StatelessWidget {
