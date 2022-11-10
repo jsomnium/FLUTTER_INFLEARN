dc355f1847194e227f559c2be8590fb849926f41
jsomnium <jsomnium@naver.com>
2022-11-10 15:27:19 +0900

    Update main.dart

--- onboarding/lib/main.dart
+++ onboarding/lib/main.dart
@@ -3,12 +3,17 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// SharedPreferences 인스턴스를 어디서든 접근 가능하도록 전역 변수로 선언
+late SharedPreferences prefs;
+
 void main() async {
   // main() 함수에서 async를 쓰려면 필요
   WidgetsFlutterBinding.ensureInitialized();
 
   // shared_preferences 인스턴스 생성
-  SharedPreferences prefs = await SharedPreferences.getInstance();
+  prefs = await SharedPreferences.getInstance();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,12 +21,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // SharedPreferences에서 온보딩 완료 여부 조회
+    // isOnboarded에 해당하는 값어서 null을 반환하는 경우 false 할당
+    bool isOnboarded = prefs.getBool("isOnboarded") ?? false;
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.getTextTheme('Jua'),
       ),
-      debugShowCheckedModeBanner: false,
-      home: OnboardingPage(),
+      home: isOnboarded ? HomePage() : OnboardingPage(),
     );
   }
 }
@@ -77,6 +85,9 @@ class OnboardingPage extends StatelessWidget {
         next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
         done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
         onDone: () {
+          // Done 클릭시 isOnboarded = true로 저장
+          prefs.setBool("isOnboarded", true);
+
           // Done 클릭시 페이지 이동
           Navigator.pushReplacement(
             context,
@@ -95,17 +106,22 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page!"),
-        centerTitle: true,
+        title: Text("HomePage"),
+        actions: [
+          // 삭제 버튼
+          IconButton(
+            onPressed: () {
+              // SharedPreferences에 저장된 모든 데이터 삭제
+              prefs.clear();
+            },
+            icon: Icon(Icons.delete),
+          )
+        ],
       ),
       body: Center(
-        child: Center(
-          child: Text(
-            "환영합니다!",
-            style: TextStyle(
-              fontSize: 24,
-            ),
-          ),
+        child: Text(
+          "환영합니다.",
+          style: TextStyle(fontSize: 28),
         ),
       ),
     );
