d7b44c1e7360b321558e31e6edcd333a0a1b9686
jsomnium <jsomnium@naver.com>
2022-11-17 15:49:30 +0900

    Fetch: AppBar

--- book_store/lib/main.dart
+++ book_store/lib/main.dart
@@ -32,7 +32,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<BookService>(
       builder: (context, bookService, child) {
-        return Scaffold();
+        return Scaffold(
+          appBar: AppBar(
+              title: Text(
+                'Book Store',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              backgroundColor: Colors.white,
+              centerTitle: false,
+              actions: [
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  padding: const EdgeInsets.only(right: 12),
+                  child: Text(
+                    'total ${bookService.bookList.length}',
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                  ),
+                )
+              ],
+              bottom: PreferredSize()),
+        );
       },
     );
   }
