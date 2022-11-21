61b010e998b1833a9a24d1ef17bf91e8368e1a1b
jsomnium <jsomnium@naver.com>
2022-11-21 18:38:12 +0900

    Fetch: context bookService

--- book_store/lib/book.dart
+++ book_store/lib/book.dart
@@ -11,6 +11,8 @@ class Book {
     required this.previewLink,
   });
 
+  // Map<String, dynamic>을 전달받아 Book 클래스 인스턴스를 반환하는 함수
+  // factory 키워드를 붙여서 생성자로 사용
   factory Book.fromJson(Map<String, dynamic> volumeInfo) {
     return Book(
       // title이 없는 경우 빈 문자열 할당
