0cf726d975c85a93ad574c6df836e187ec67e44e
jsomnium <jsomnium@naver.com>
2022-11-21 19:03:40 +0900

    Fetch: GetX(1)

--- book_store/lib/book_service.dart
+++ book_store/lib/book_service.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 
 import 'book.dart';
 
-class BookService extends ChangeNotifier {
+class BookService extends GetxController {
   // 책 목록
-  List<Book> bookList = [];
+  RxList<Book> bookList = <Book>[].obs;
 
   /// 검색어로 책 정보 불러오기
   void getBookList(String q) async {
     bookList.clear(); // 기존에 들어있는 데이터 초기화
 
     // API 호출
-    Response res = await Dio().get(
+    var res = await Dio().get(
       "https://www.googleapis.com/books/v1/volumes?q=$q&startIndex=0&maxResults=40",
     );
     List items = res.data["items"]; // items 접근
@@ -21,8 +21,5 @@ class BookService extends ChangeNotifier {
       Book book = Book.fromJson(volumeInfo); // Map -> Book
       bookList.add(book); // Book 추가
     }
-
-    // 화면 갱신
-    notifyListeners();
   }
 }
--- book_store/lib/main.dart
+++ book_store/lib/main.dart
@@ -43,99 +43,104 @@ class HomePage extends StatelessWidget {
     }
   }
 
+  final BookService bookService = Get.put(BookService())
+
   @override
   Widget build(BuildContext context) {
     final bookService = context.watch<BookService>();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: false,
-        title: Text(
-          "Book Store",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        actions: [
-          Container(
-            alignment: Alignment.bottomCenter,
-            padding: const EdgeInsets.only(right: 12),
-            child: Text(
-              "total ${bookService.bookList.length}",
+    return Obx( // 관찰해야 할 대상이 있는 위젯을 Obx 라고 함
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            centerTitle: false,
+            title: Text(
+              "Book Store",
               style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
                 color: Colors.black,
-                fontSize: 16,
               ),
             ),
-          ),
-        ],
-
-        /// AppBar의 Bottom은 항상 PreferredSize 위젯으로 시작해야합니다.
-        bottom: PreferredSize(
-          preferredSize: Size(double.infinity, 72),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: searchController,
-              decoration: InputDecoration(
-                hintText: "원하시는 책을 검색해주세요.",
-                // 테두리
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
+            actions: [
+              Container(
+                alignment: Alignment.bottomCenter,
+                padding: const EdgeInsets.only(right: 12),
+                child: Text(
+                  "total ${bookService.bookList.length}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
                 ),
+              ),
+            ],
 
-                /// 돋보기 아이콘
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    // 돋보기 아이콘 클릭
+            /// AppBar의 Bottom은 항상 PreferredSize 위젯으로 시작해야합니다.
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 72),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: searchController,
+                  decoration: InputDecoration(
+                    hintText: "원하시는 책을 검색해주세요.",
+                    // 테두리
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+
+                    /// 돋보기 아이콘
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () {
+                        // 돋보기 아이콘 클릭
+                        search(bookService);
+                      },
+                    ),
+                  ),
+                  onSubmitted: (v) {
+                    // 엔터를 누르는 경우
                     search(bookService);
                   },
                 ),
               ),
-              onSubmitted: (v) {
-                // 엔터를 누르는 경우
-                search(bookService);
-              },
             ),
           ),
-        ),
-      ),
-      body: bookService.bookList.isEmpty
-          // bookList가 비어있는 경우
-          ? Center(
-              child: Text(
-                "검색어를 입력해 주세요",
-                style: TextStyle(
-                  fontSize: 21,
-                  color: Colors.grey,
-                ),
-              ),
-            )
-          // bookList 보여주기
-          : ListView.builder(
-              itemCount: bookService.bookList.length,
-              itemBuilder: (context, index) {
-                Book book = bookService.bookList[index];
-                return ListTile(
-                  leading: Image.network(
-                    book.thumbnail,
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
+          body: bookService.bookList.isEmpty
+              // bookList가 비어있는 경우
+              ? Center(
+                  child: Text(
+                    "검색어를 입력해 주세요",
+                    style: TextStyle(
+                      fontSize: 21,
+                      color: Colors.grey,
+                    ),
                   ),
-                  title: Text(book.title),
-                  subtitle: Text(book.subtitle),
-                  onTap: () {
-                    // 클릭시 previewLink 띄우기
-                    Uri uri = Uri.parse(book.previewLink);
-                    launchUrl(uri);
+                )
+              // bookList 보여주기
+              : ListView.builder(
+                  itemCount: bookService.bookList.length,
+                  itemBuilder: (context, index) {
+                    Book book = bookService.bookList[index];
+                    return ListTile(
+                      leading: Image.network(
+                        book.thumbnail,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      title: Text(book.title),
+                      subtitle: Text(book.subtitle),
+                      onTap: () {
+                        // 클릭시 previewLink 띄우기
+                        Uri uri = Uri.parse(book.previewLink);
+                        launchUrl(uri);
+                      },
+                    );
                   },
-                );
-              },
-            ),
+                ),
+        );
+      }
     );
   }
 }
